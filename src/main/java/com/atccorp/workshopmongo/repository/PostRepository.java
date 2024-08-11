3ba3f2470658f9250a18b827baa4ff417e6e27e3
Jose Carlos Junior <josecarlosjr.atc@gmail.com>
2024-08-11 17:05:05 -0300

    Simple query with query methods

--- src/main/java/com/atccorp/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/atccorp/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.atccorp.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.atccorp.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
